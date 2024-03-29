dafdc7c91b75e2ab1cd8d00f5484b001068d1192
maulikam <mdave.5191@gmail.com>
2024-03-30 04:09:24 +0530

    init project setup

---
+++ options-explorer/src/main/java/com/rev/analitics/algo/optionsexplorer/OptionsExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.rev.analitics.algo.optionsexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionsExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptionsExplorerApplication.class, args);
+	}
+
+}
---
+++ options-explorer/src/test/java/com/rev/analitics/algo/optionsexplorer/OptionsExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rev.analitics.algo.optionsexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptionsExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
